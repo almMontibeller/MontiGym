3906e1052d6481b67dccd2811c7573c9e5ab6c05
almMontibeller <153998768+almMontibeller@users.noreply.github.com>
2024-09-01 13:22:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write("test")
+
+training, calendar, exercises, results = st.tabs(["Treinos", "Calendário", "Exercícios", "Resultados"])
+
+with training:
+    for i in range(5):
+        st.subheader(f"exercicio {i}")
+        st.write("x series de y repetições")
+        st.write("placeholder pro video e ultimas x cargas")
+        st.number_input(f"Carga executada (kg) (somente variáveis) {i}")
+        st.checkbox(f"Each side {i}", value=False)
+        st.number_input(f"Carga acessório (kg) (fixa pra cada exercicio/instrumento){i}", value=10)
+        st.divider
